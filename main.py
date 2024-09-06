3705a45b2989e6c4d82e0215a69b856aa0896f5a
Anagh Neginhal <166278457+NotSomeoneOrMe@users.noreply.github.com>
2024-09-06 18:03:05 +0000

    Added rest of ships

--- main.py
+++ main.py
@@ -21,18 +21,46 @@ class Faction(Enum):
     YssarilTribes = 17
 
 class Ship():
-    def __init__(self, hitValue:int, attackTimes:int, sustainDamage:bool, antiFighterHitValue:int, antiFighterAttackTimes:int, factions:list):
+    def __init__(self, hitValue:int, attackTimes:int, sustainDamage:bool, capacity:int, antiFighterHitValue:int, antiFighterAttackTimes:int, factions:list):
         self.hitValue = hitValue
         self.attackTimes = attackTimes
         self.sustainDamage = sustainDamage
         self.antiFighterHitValue = antiFighterHitValue
         self.antiFighterAttackTimes = antiFighterAttackTimes
+        self.capacity = capacity
         if (factions[0] == Faction.All):
-            aviableFactions = list(Faction)
+            availableFactions = list(Faction)
             for faction in factions:
-                aviableFactions.remove(faction)
+                availableFactions.remove(faction)
         else:
-            aviableFactions = factions
-        self.factions = aviableFactions
+            availableFactions = factions
+        self.factions = availableFactions
 
-fighter = Ship(9, 1, False, None, 0, [Faction.All])
+fighter1 = Ship(9, 1, False, 0, None, 0, [Faction.All, Faction.NaaluCollective])
+fighter2 = Ship(8, 1, False, 0, None, 0, [Faction.All, Faction.NaaluCollective])
+
+naaluFighter1 = Ship(8, 1, False, 0, None, 0, [Faction.All, Faction.NaaluCollective])
+naaluFighter1 = Ship(7, 1, False, 0, None, 0, [Faction.All, Faction.NaaluCollective])
+
+destroyer1 = Ship(9, 1, False, 0, 9, 2, [Faction.All])
+destroyer2 = Ship(8, 1, False, 0, 6, 3, [Faction.All])
+
+
+cruiser1 = Ship(7, 1, False, 0, None, 0, [Faction.All])
+cruiser2 = Ship(6, 1, False, 1, None, 0, [Faction.All])
+
+
+dreadnought1 = Ship(5, 1, True, 1, None, 0, [Faction.All, Faction.L1Z1XMindnet, Faction.SardakkNorr])
+mindsetDreadnought1 = Ship(5, 1, True, 2, None, 0, [Faction.L1Z1XMindnet])
+mindsetDreadnought2 = Ship(4, 1, True, 2, None, 0, [Faction.L1Z1XMindnet])
+
+
+carrier1 = Ship(9, 1, False, 4, None, 0, [Faction.All, Faction.FederationOfSol])
+carrier2 = Ship(9, 1, False, 6, None, 0, [Faction.All, Faction.FederationOfSol])
+solCarrier1 = Ship(9, 1, False, 6, None, 0, [Faction.FederationOfSol])
+solCarrier2 = Ship(9, 1, True, 8, None, 0, [Faction.FederationOfSol])
+
+floatingFactory1 = Ship(None, None, False, 4, None, 0, [Faction.ClanOfSarr])
+floatingFactory2 = Ship(None, None, False, 5, None, 0, [Faction.ClanOfSarr])
+
+warSun2 = Ship(3, 3, True, 6, None, 0, [Faction.All])
